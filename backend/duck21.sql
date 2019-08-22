5289a06ec28c3f028d2d60da55c9db8cf8ea7ba2
ChetTian <724133160@qq.com>
2019-08-22 14:34:49 +0800

    add sql file

---
+++ backend/duck21.sql
@@ -0,0 +1,42 @@
+use study_forum;
+
+create table user(
+    user_id int primary key,
+    password varchar(64) not null,
+    nickname varchar(64) not null,
+    age int,
+    school varchar(128),
+    head varchar(128),
+    profile varchar(256),
+    status varchar(256)
+)
+
+create table posting(
+    postingid int primary key auto_increment,
+    posting_user_id int not null,
+    posting_time datetime not null ,
+    reply_time datetime not null ,
+    reply_num int not null,
+    theme varchar(256) not null,
+    posting_content varchar(16384) not null,
+    category varchar(256) not null,
+    posting_thumb_num int not null
+)
+
+create table reposting(
+    reposting_id int primary key auto_increment,
+    reply_id int not null,
+    main_posting_id int not null,
+    reposting_user_id int not null,
+    repostring_time datetime not null,
+    reposting_content varchar(16384) not null ,
+    reposting_thumb_num int not null
+)
+
+create table notify(
+    notify_id int primary key,
+    notify_user_id int not null,
+    notify_content varchar(16384),
+    notify_time datetime not null,
+    notify_status varchar(256)
+)
