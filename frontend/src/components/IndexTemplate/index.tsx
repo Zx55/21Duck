9ba72e32d7bc4143b640b361b87f8a5a18f79560
Zx55 <czr.cn.525@gmail.com>
2019-08-24 17:16:18 +0800

    frontend: fix abnormal page displaying

---
+++ frontend/src/components/IndexTemplate/index.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+
+import api from '../../api';
+import PostList from '../../components/PostList';
+import Loading from '../../components/Loading';
+
+import { Pagination } from 'antd';
+
+import { PostItem } from '../../types';
+import { ParamList } from '../../types';
+
+import './Template.css';
+
+
+export interface IndexTemplateProps {
+    name: string;
+    category: string;
+};
+
+export default (props: IndexTemplateProps) => {
+    const [posts, setPosts] = useState(new Array<PostItem>());
+    const [postNum, setPostNum] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [current, setCurrent] = useState(1);
+
+    const getPosts = (page: string): void => {
+        setLoading(true);
+
+        const params: ParamList = [{
+            key: 'page',
+            value: page,
+        }]/*, {
+            key: 'category',
+            value: props.category,
+        }];*/
+
+        api.post.list(params).then((response) => {
+            const posts: Array<PostItem> = response.data;
+            setPostNum(posts[0].posting_num);
+            setPosts(posts);
+            setLoading(false);
+        }).catch(err => console.log(err));
+    };
+
+    useEffect(() => {
+        getPosts('0');
+    }, []);
+
+    const handlePageChange = (page: number): void => {
+        setCurrent(page);
+        getPosts((page - 1).toString());
+    }
+
+    return (
+        <div className={`${props.name}-root`}>
+            <div>{props.name}</div>
+            {loading ? <Loading /> : <PostList posts={posts} />}
+            <Pagination
+                className={`${props.name}-pagination`}
+                current={current}
+                defaultPageSize={15}
+                size='small'
+                hideOnSinglePage
+                total={postNum}
+                showQuickJumper
+                onChange={(page: number) => handlePageChange(page)}
+            />
+        </div>
+    );
+};
--- frontend/src/pages/problems/index.tsx
+++ frontend/src/pages/problems/index.tsx
@@ -1,71 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-import api from '../../api';
-import PostList from '../../components/PostList';
-import Loading from '../../components/Loading';
-
-import { Pagination } from 'antd';
-
-import { PostItem } from '../../types';
-import { ParamList } from '../../types';
+import Template from '../../components/IndexTemplate';
 
 import './Problems.css';
 
 
-export default () => {
-    const [posts, setPosts] = useState(new Array<PostItem>());
-    const [postNum, setPostNum] = useState(0);
-    const [loading, setLoading] = useState(true);
-    const [current, setCurrent] = useState(1);
-
-    const getPosts = (page: string): void => {
-        setLoading(true);
-
-        /*
-        const params: ParamList = [{
-            key: 'page',
-            value: page,
-        }, {
-            key: 'category',
-            value: '2',
-        }];
-        */
-        const params: ParamList = [{
-            key: 'page',
-            value: page,
-        }];
-
-        api.post.list(params).then((response) => {
-            const posts: Array<PostItem> = response.data;
-            setPostNum(posts[0].posting_num);
-            setPosts(posts);
-            setLoading(false);
-        }).catch(err => console.log(err));
-    }
-
-    useEffect(() => {
-        getPosts('0');
-    }, []);
-
-    const handlePageChange = (page: number): void => {
-        setCurrent(page);
-        getPosts((page - 1).toString());
-    }
-
-    return (
-        <div className='problems-root'>
-            <div>Problems</div>
-            {loading ? <Loading /> : <PostList posts={posts} />}
-            <Pagination
-                className='problem-pagination'
-                current={current}
-                defaultPageSize={15}
-                size='small'
-                hideOnSinglePage
-                total={postNum}
-                showQuickJumper
-                onChange={(page: number) => handlePageChange(page)}
-            />
-        </div>
-    );
-}
+export default () => (
+    <Template name='problem' category='2' />
+);
