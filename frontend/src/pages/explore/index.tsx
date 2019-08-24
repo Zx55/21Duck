410e89ac582b5908939ed5c067ac78568543daa3
Limimimir <linmingrui0214@163.com>
2019-08-24 12:01:59 +0800

    frontend:add banner

--- frontend/src/pages/explore/index.tsx
+++ frontend/src/pages/explore/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import { Layout } from 'antd';
+import Routes from '../Routes';
+import Banner from '../../components/Banner'
+
+import './explore.css'
+
+export interface BannerProps {
+    className: string;
+}
 
 export default () => (
-    <div>
-        Explore
+    <div className = 'banner'>
+        <Banner className = 'switch-banner'/>
     </div>
 );
