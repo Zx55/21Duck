ac1baee4143fb477db7edd94eb1f1fff5d8afcfd
kiaia <898134692@qq.com>
2019-08-28 09:08:51 +0800

    Signed-off-by: kiaia <898134692@qq.com>

--- frontend/src/pages/User/index.tsx
+++ frontend/src/pages/User/index.tsx
@@ -1,8 +1,62 @@
 import React from 'react';
+import { Avatar } from 'antd'
+import AutoAvator from '../../components/AutoAvator'
+import { Card } from 'antd';
 
+import Template from '../../components/PageListTemplate';
+
+import './Problems.css';
+import { Tabs, Icon } from 'antd';
+import src from '*.bmp';
+
+const { TabPane } = Tabs;
 
 export default () => (
-    <div>
-        User
+    <div >
+         <Card
+         
+    hoverable
+    style={{ width: 1400 }}
+    cover={<img alt="example" src="https://pic1.zhimg.com/v2-99413d2b2721b9fc33280ca041902aac_b.jpg" />}
+  >
+          
+            <div className="test">
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            </div>
+            <p className='name'>kiaia</p>
+            <p className='sign'>happy coding</p>
+            
+        </Card>
+        <Tabs defaultActiveKey="2">
+    <TabPane
+      tab={
+        <span>
+          <Icon type="apple" />
+          我的主贴
+        </span>
+      }
+      key="1"
+    >
+      <div>
+            <Template name='problem' category='2' />
+        </div>
+    </TabPane>
+    <TabPane
+      tab={
+        <span>
+          <Icon type="android" />
+          我的回复
+        </span>
+      }
+      key="2"
+    >
+      <div>
+            <Template name='problem' category='2' />
+        </div>
+    </TabPane>
+  </Tabs>
+        
     </div>
+    
+    
 );
--- frontend/src/reducers/index.ts
+++ frontend/src/reducers/index.ts
@@ -1,3 +1,5 @@
+
+
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import sessionStorage from 'redux-persist/lib/storage/session';
