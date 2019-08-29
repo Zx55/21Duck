0b0b4f4b49eb5ff49200ae339c3d6992b7acda25
Limimimir <linmingrui0214@163.com>
2019-08-29 16:54:34 +0800

    frontend: 修改了主页，还没改完-tree

--- frontend/src/pages/Explore/index.tsx
+++ frontend/src/pages/Explore/index.tsx
@@ -1,15 +1,75 @@
 import React from 'react';
 
+import { Carousel, Card } from 'antd';
+
 import Banner from '../../components/ExploreBanner'
 
-import './Explore.css'
+import './Explore.css';
 
 export interface BannerProps {
     className: string;
 }
 
 export default () => (
-    <div className = 'banner'>
-        <Banner className = 'switch-banner'/>
-    </div>
+    <div>
+        <div className='banner'>
+            <Banner className='switch-banner' />
+        </div>
+        <div className='side-banner'>
+            <Carousel
+                autoplay
+                style={{
+                    textAlign: "center",
+                    height: "450px",
+                    lineHeight: "460px",
+                    background: "#3399ff",
+                    overflow: "hidden",
+                }}
+            >
+                <div color="#FFFFFF">
+                    1
+                </div>
+                <div>
+                    2
+                </div>
+                <div>
+                    3
+                </div>
+                <div>
+                    4
+                </div>
+            </Carousel>
+        </div>
+        <div>
+            <div className="container">
+                <Card
+                    title="Default size card"
+                    style={{ 
+                        width: 300,
+                        borderRadius: "0.8ch",
+                        margin: "10px",
+                    }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+                <Card 
+                    title="Default size card"
+                    style={{ 
+                        width: 300,
+                        borderRadius: "0.8ch",
+                        margin: "10px",
+                    }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+                <Card title="Default size card" style={{ width: 300 }}>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                    <p>Card content</p>
+                </Card>
+            </div>
+        </div>
+    </div >
 );
