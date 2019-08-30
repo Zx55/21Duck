1407a9d0f21f1c5b99944f879ecf00be216f54a6
kiaia <898134692@qq.com>
2019-08-30 15:10:39 +0800

    fixed the page-not-found bug in explore page

--- frontend/src/components/ExploreTemp/index.tsx
+++ frontend/src/components/ExploreTemp/index.tsx
@@ -12,46 +12,42 @@ import { Link } from 'react-router-dom';
 
 const { Meta } = Card;
 
-const categoryMap={
-    1:"xhat",
-    2:"problems",
-    3:"courses",
-    4:"campus"
-}
+var m = new Map([['1', "chat"], [ '2', "problems"],[ '3', "courses"],['4', "campus"]]);
+console.log(m.get('Michael')); 
 export interface PostProps {
-    category:String;
+    category: string;
     post: IPost;
     loading: boolean;
     detail: boolean;
 };
 
 export default (props: PostProps) => (
-    
-    <Link to={`/problems/${props.post.posting_id}`}>
-    <Card
-        className={cx('cardgroup')}
-        hoverable
-        size="small"
-        title={props.post.theme}
-        
-    >
-        <Skeleton
-            loading={props.loading}
-            active
+
+    <Link to={`/${m.get(props.category)}/${props.post.posting_id}`}>
+        <Card
+            className={cx('cardgroup')}
+            hoverable
+            size="small"
+            title={props.post.theme}
+
         >
+            <Skeleton
+                loading={props.loading}
+                active
+            >
                 <span className='user-nickname'>
                     {props.post.user_nickname}
                 </span>
                 <span>
-                <Tooltip className='post-created-time' title={props.post.formated_posting_time}>
-                    <p>
-                        {moment(props.post.formated_posting_time,
-                            'YYYY-MM-DD HH:mm:ss').fromNow()}
-                    </p>
-                </Tooltip>
+                    <Tooltip className='post-created-time' title={props.post.formated_posting_time}>
+                        <p>
+                            {moment(props.post.formated_posting_time,
+                                'YYYY-MM-DD HH:mm:ss').fromNow()}
+                        </p>
+                    </Tooltip>
                 </span>
-            
-        </Skeleton>
-    </Card>
+
+            </Skeleton>
+        </Card>
     </Link>
 );
