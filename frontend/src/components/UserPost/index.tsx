d78258959afd210583b8034a12884c88e51678d2
Zx55 <czr.cn.525@gmail.com>
2019-08-31 10:34:32 +0800

    frontend: fix #16

--- frontend/src/components/UserPost/index.tsx
+++ frontend/src/components/UserPost/index.tsx
@@ -17,28 +17,48 @@ export interface PostProps {
     detail: boolean;
 };
 
-export default (props: PostProps) => (
-    <Card
-        className={cx('user-post', props.detail && 'post-detail')}
-        hoverable
-        bordered={false}
-        bodyStyle={{padding:"10px 20px"}}
-        >
-        <Skeleton
-            loading={props.loading}
-            active
-        >
-            <Link to={`/problems/${props.post.posting_id}`}>
-                {props.post.theme}
-            </Link>
-            <span style={{ float: "right" }}>
-                <Tooltip className='post-created-time' title={props.post.formated_posting_time}>
-                    <span>
-                        {moment(props.post.formated_posting_time,
-                            'YYYY-MM-DD HH:mm:ss').fromNow()}
-                    </span>
-                </Tooltip>
-            </span>
-        </Skeleton>
-    </Card>
-);
+export default (props: PostProps) => {
+    const getCategory = (categoryId: number) => {
+        console.log(categoryId);
+        switch (categoryId) {
+            case 1:
+                return 'chat';
+            case 2:
+                return 'problems';
+            case 3:
+                return 'courses';
+            case 4:
+                return 'campus';
+            case 5:
+                return 'resources';
+            default:
+                return '';
+        }
+    };
+
+    return (
+        <Card
+            className={cx('user-post', props.detail && 'post-detail')}
+            hoverable
+            bordered={false}
+            bodyStyle={{ padding: "10px 20px" }}
+            >
+            <Skeleton
+                loading={props.loading}
+                active
+            >
+                <Link to={`/${getCategory(props.post.category_id)}/${props.post.posting_id}`}>
+                    {props.post.theme}
+                </Link>
+                <span style={{ float: "right" }}>
+                    <Tooltip className='post-created-time' title={props.post.formated_posting_time}>
+                        <span>
+                            {moment(props.post.formated_posting_time,
+                                'YYYY-MM-DD HH:mm:ss').fromNow()}
+                        </span>
+                    </Tooltip>
+                </span>
+            </Skeleton>
+        </Card>
+    );
+};
